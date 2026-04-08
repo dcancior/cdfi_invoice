690ecb0c3895d90721d11927d966b1ce4469888a
David Cancio Reyes <d_cancio.com>
2026-04-08 12:46:01 -0600

    mejorando el password dialogo para romper conciliaciones los miembros del grupo de romper conciliaciones _ ok 2

--- __manifest__.py
+++ __manifest__.py
@@ -16,6 +16,9 @@
         'sale','account','purchase', 'base_vat'
     ],
     'data': [
+
+        'security/security.xml',  # <-- DEBE IR AQUÍ (en la sección data)
+        
         'data/catalogo.unidad.medida.csv',
         'data/catalogo.forma.pago.csv',
         'data/catalogo.uso.cfdi.csv',
@@ -44,11 +47,9 @@
     ],
     'assets': {
         'web.assets_backend': [
+            'cdfi_invoice/static/src/xml/password_dialog.xml',
             'cdfi_invoice/static/src/js/password_dialog.js',
             'cdfi_invoice/static/src/js/account_payment_field_extend.js',
-        ],
-    'web.assets_qweb': [
-        'cdfi_invoice/static/src/xml/password_dialog.xml',
         ],
     },
     'images': ['static/description/banner.jpg'],
