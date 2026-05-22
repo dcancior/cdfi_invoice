09363c1595180ba8cc227bd7ed416c19aeda7df6
David Cancio Reyes <d_cancio.com>
2026-05-22 13:50:46 -0600

    pruebas 4

--- __manifest__.py
+++ __manifest__.py
@@ -37,7 +37,6 @@
         'views/purchase_view.xml',
         'views/account_journal_view.xml',
         'views/res_currency.xml',
-        'report/invoice_report.xml',
         'report/invoice_report_custom.xml',
         'report/payment_report.xml',
         'data/mail_template_data.xml',
